aa67819905146816a730c8b6792c1d1a43dd97f3
Arthur Ghizi <arthurghizi22@gmail.com>
2025-06-05 16:59:17 -0300

    tela de cadastro iniciada

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,7 +15,7 @@ export default function Form({
 	return (
 		<form
 			onSubmit={handleSubmit}
-			className="bg-white dark:bg-gray-900 shadow-md rounded-xl p-8 w-full max-w-md mx-auto transition-colors duration-300">
+			className="bg-white dark:bg-gray-900 shadow-md rounded-xl p-8 w-full max-w-md mx-auto mb-4 mt-4 transition-colors duration-300">
 			{logo && (
 				<img src={logo} alt="Logo" className="h-35 mx-auto dark:invert" />
 			)}
@@ -23,25 +23,26 @@ export default function Form({
 				{title}
 			</h2>
 
-			{fields.map((field, index) => (
-				<div key={index} className="mb-4">
-					<label
-						className="block text-sm font-medium mb-1 dark:text-white"
-						htmlFor={field.name}>
-						{field.label}
-					</label>
-					<input
-						type={field.type}
-						id={field.name}
-						name={field.name}
-						value={values[field.name] || ""}
-						onChange={handleChange}
-						placeholder={field.placeholder}
-						className="placeholder:text-gray-600 w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-700 dark:placeholder:text-gray-400 dark:focus:ring-blue-200 text-black dark:text-white"
-						required={field.required}
-					/>
-				</div>
-			))}
+			{fields &&
+				fields.map((field, index) => (
+					<div key={index} className="mb-4">
+						<label
+							className="block text-sm font-medium mb-1 dark:text-white"
+							htmlFor={field.name}>
+							{field.label}
+						</label>
+						<input
+							type={field.type}
+							id={field.name}
+							name={values[field.name] || ""}
+							value={values[field.name] || ""}
+							onChange={handleChange}
+							placeholder={field.placeholder}
+							className="placeholder:text-gray-600 w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-700 dark:placeholder:text-gray-400 dark:focus:ring-blue-200 text-black dark:text-white"
+							required={field.required}
+						/>
+					</div>
+				))}
 
 			<Buttons
 				text={"Entrar"}
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import Container from "../../components/Container";
+import Form from "../../components/Form";
+import ThemeToggle from "../../components/Theme/ThemeToggle";
+import logo from "../../assets/images/logo.svg";
+import { useForm } from "../../hooks/useForm";
 
 const Register = () => {
-	return <div>Register</div>;
+	const fields = [
+		{
+			label: "Nome",
+			type: "text",
+			name: "name",
+			required: true,
+			placeholder: "Digite seu nome",
+		},
+		{
+			label: "E-mail",
+			type: "text",
+			name: "email",
+			required: true,
+			placeholder: "Digite seu e-mail",
+		},
+		{
+			label: "Senha",
+			type: "password",
+			name: "password",
+			required: true,
+			placeholder: "Digite sua senha",
+		},
+		{
+			label: "Confirmar senha",
+			type: "password",
+			name: "passwordConfirm",
+			required: true,
+			placeholder: "Confirme sua senha",
+		},
+	];
+
+	const { values, handleChange, handleSubmit, resetForm } = useForm({});
+
+	return (
+		<>
+			<Container centered>
+				<Form
+					title="Cadastrar"
+					text={"Já possui uma conta?"}
+					handleChange={handleChange}
+					handleSubmit={handleSubmit}
+					fields={fields}
+					values={values}
+					path={"/login"}
+					pathTitle={"Entrar"}
+					logo={logo}></Form>
+				<div className="absolute bottom-4 left-4">
+					<ThemeToggle />
+				</div>
+			</Container>
+		</>
+	);
 };
 
 export default Register;
