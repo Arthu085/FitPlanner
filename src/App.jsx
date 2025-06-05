c88ab634d2ec2bc9f7a519249e96c826dad9740c
Arthur Ghizi <arthurghizi22@gmail.com>
2025-06-05 09:48:42 -0300

    iniciado o roteamento das telas principais

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
 
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import NotFound from "./pages/NotFound/NotFound";
+
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">Hello World</h1>
+			<Router>
+				<Routes>
+					<Route path="/login" element={<Login />}></Route>
+					<Route path="/register" element={<Register />}></Route>
+					<Route path="/" element={<Navigate to="/login" />}></Route>
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Router>
 		</>
 	);
 }
