abe7c6b9fd2ca63adf9fb2b578983cafe1da5497
Arthur Ghizi <arthurghizi22@gmail.com>
2025-06-06 13:56:44 -0300

    footer adicionado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+	return (
+		<footer className="w-full py-4 text-center  shadow-inner">
+			<p className="text-base font-bold text-black dark:text-gray-300">
+				© 2025 FitPlanner
+			</p>
+		</footer>
+	);
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Container from "../../components/Container";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
 		<>
 			<Container>
 				<Header />
+				<Footer />
 			</Container>
 		</>
 	);
